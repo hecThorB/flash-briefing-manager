301534a9456dd498705923a1d89b633a5abdd9a4
hecThorB <hectorobeltran@gmail.com>
2018-05-30 22:53:43 -0700

    Update config.js

--- example/config.js
+++ example/config.js
@@ -1,8 +1,8 @@
 const config = {};
 
 config.region = "us-east-1",
-config.accessKeyId = "AKIAIHROKHIKMBDDSWTA",
-config.secretAccessKey = "gqAXTV2yBgC6SfuylFyRXzyBaUJC0tIHKSFt7nVC"
+config.accessKeyId = "",
+config.secretAccessKey = ""
 config.bucketName = "news-brief-example"
 
 module.exports = config;
